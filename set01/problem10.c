5fead7f7d8106458599c2c458748a9c60af16ffe
Yaminie V <141309225+yaminie-1234@users.noreply.github.com>
2024-03-01 20:13:04 +0530

    test

--- set01/problem10.c
+++ set01/problem10.c
@@ -7,9 +7,9 @@ void output(char *string1, char *string2, int result);
 
 int main()
 {
-  int result;
   char string1[100], string2[100];
   input_two_strings(string1, string2);
+  int result;
   result = stringcompare(string1, string2);
   output(string1, string2, result);
   return 0;
@@ -26,16 +26,20 @@ void input_two_strings(char *string1, char *string2)
 int stringcompare(char *string1, char *string2)
 {
   int i = 0;
-  for (i = 0; string1[i] != '\0' && string2[i] != '\0'; i + 1)
+  for (i = 0; string1[i] != '\0' && string2[i] != '\0'; i++)
   {
-    if (string1[i] == string2[i])
-      return 0;
+    if (string1[i] == string2[i] ){
+      i++;
 
-    if (string1[i] > string2[i])
+    }
+      
+    else if (string1[i] > string2[i])
       return 1;
 
-      else 
-      return 2;
+      else {
+        return -1;
+      }
+      
     }
 }
     void output(char *string1, char *string2, int result)
